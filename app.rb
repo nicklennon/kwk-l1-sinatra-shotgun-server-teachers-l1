bc599c44733eda2b069dc486bb692c90df9e1481
nicklennon <nicklennon.53@gmail.com>
2018-07-31 18:34:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Now I'm using shotgun"
   end
 
+  get '/Nick' do
+    erb :nick
+  end
 end
